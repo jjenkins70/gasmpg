cd3101e1d508c00a878e8abb2841479b738654d4
Jeremiah Jenkins <JJenkins70@gmail.com>
2012-02-09 22:12:42 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gasmpg::Application.config.secret_token = '98e6866d3f5f38ba220e8617cee7d21f684c38774d3fad63f38408b321d734fd7c8c1ec5c3a65d522201a843ee3be088fb6519ea1015a680f801a3896ed037fc'
